18227d21172b23fa15f0e9eab027752dbb45c469
GodoFredo <godofredo720@hotmail.com>
2015-06-04 11:59:37 -0500

    Nuevo estilo

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,44 +1,48 @@
-(function(){
-  'use strict';
+(function($){
+  $(function(){
+    'use strict';
 
-  // Search Menu
-  $('.Menu-openSearch').on('click', Formulario);
+    // Search Menu
+    $('.Menu-openSearch').on('click', Formulario);
 
-  function Formulario(){
-    $('.Menu-form').removeClass('close');
-  }
+    function Formulario(){
+      $('.Menu-form').removeClass('close');
+    }
 
-  $('.Menu-closeSearch').on('click', function(){
-   $('.Menu-form').addClass('close');
-  });
-
-  // Menu
-  $(".button-collapse").sideNav();
-
-  // Search
-    $('#Search').ghostHunter({
-      results: '#Search-results',
-      zeroResultsInfo : false,
-      displaySearchInfo   : false,
-      result_template : "<a class='Menu-resultForm' href='{{link}}'>{{title}}</a>",
-      onKeyUp         : true
+    $('.Menu-closeSearch').on('click', function(){
+     $('.Menu-form').addClass('close');
     });
 
-  // Main post ul li
-   $('.Post ul li').prepend('<i class="mdi-image-brightness-1"></i>' );
+    // Menu
+    $(".button-collapse").sideNav();
 
-  // Menu Open & close Desktop
-  $('.Menu-openDesktop').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-    }
-  );
+    // Search
+      $('#Search').ghostHunter({
+        results: '#Search-results',
+        zeroResultsInfo : false,
+        displaySearchInfo   : false,
+        result_template : "<a class='Menu-resultForm' href='{{link}}'>{{title}}</a>",
+        onKeyUp         : true
+      });
 
-  // Add Script Video responsive
-  $('.Post-content').fitVids();
+    // Main post ul li
+     $('.Post ul li').prepend('<i class="mdi-image-brightness-1"></i>' );
 
-})();
+    // Menu Open & close Desktop
+    $('.Menu-openDesktop').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+      }
+    );
+
+    // Add Script Video responsive
+    $('.Post-content').fitVids();
+
+    // $('.carajo').pushpin({ top: $('.ptm').offset().top });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
