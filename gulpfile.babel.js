f494fb7c144aa9c8fde694a049e91b2637989de2
godofredo <hell@godofredo.ninja>
2016-09-19 13:07:32 -0500

    update (package.json - webpack - gulp)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,46 +1,41 @@
 'use strict';
 
-const gulp         = require('gulp'); // Gulp of-course
-
-// CSS related plugins.
-const sass         = require('gulp-sass'); // Gulp pluign for Sass compilation.
-// const uglifycss    = require('gulp-uglifycss'); // Minifies CSS files.
-const autoprefixer  = require('gulp-autoprefixer'); // Autoprefixing magic.
-const sourcemaps   = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in
+const gulp 			= require('gulp'),
+	sass 			= require('gulp-sass'),
+	sourcemaps 		= require('gulp-sourcemaps'),
+	postcss      	= require('gulp-postcss'),
+	autoprefixer 	= require('autoprefixer');
 
 
 gulp.task('sass', () => {
-    gulp.src('./src/sass/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        errLogToConsole: true,
-        outputStyle: 'compressed',
-        // precision: 10
-    })
-    .on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./assets/css/'));
+	gulp.src('./src/sass/main.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		errLogToConsole: true,
+		outputStyle: 'compressed',
+		// precision: 10
+	}).on('error', sass.logError))
+	.pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('./assets/css/'));
 
-    gulp.src('./src/sass/themes/**/*.scss')
-    .pipe(sass({
-        errLogToConsole: true,
-        outputStyle: 'compressed'
-    })
-    .on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./assets/css/themes'));
+	gulp.src('./src/sass/themes/**/*.scss')
+	.pipe(sass({
+		errLogToConsole: true,
+		outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+	.pipe(gulp.dest('./assets/css/themes'));
 });
 
 gulp.task('sass-compress', () => {
-    gulp.src('./src/sass/main.scss')
-    .pipe(sass({
-        errLogToConsole: true,
-        outputStyle: 'compressed',
-    })
-    .on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./assets/css/'));
+	gulp.src('./src/sass/main.scss')
+	.pipe(sass({
+		errLogToConsole: true,
+		outputStyle: 'compressed',
+	}).on('error', sass.logError))
+	.pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+	.pipe(gulp.dest('./assets/css/'));
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,39 +7,25 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./src/js/app.js",
   module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: ['es2015'],
-          plugins: ['add-module-exports', 'transform-class-properties', 'transform-decorators-legacy']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loaders: [          
-          'style',
-          'css',
-          'resolve-url',
-          'autoprefixer?browsers=last 3 versions',
-          'sass?outputStyle=expanded',
-        ]
-      },
-      {
-        test: /\.(woff2?|ttf|eot|svg)$/,
-        loader: 'url?limit=10000'
-      }
-    ]
+	loaders: [
+	  {
+		test: /\.js?$/,
+		exclude: /(node_modules|bower_components)/,
+		loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+		query: {
+		  presets: ['es2015'],
+		  plugins: ['add-module-exports']
+		}
+	  }
+	]
   },
   output: {
-    path: path.join(__dirname, "assets/js"),    
-    filename: "bundle.min.js"
+	path: path.join(__dirname, "assets/js"),    
+	filename: "bundle.min.js"
   },
   plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+	new webpack.optimize.DedupePlugin(),
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
