177b5268e313c4c8eca47eedf85fd9e22fd7e143
GodoFredo <hell@godofredo.ninja>
2017-07-08 10:50:16 -0500

    add line numbers for code and better facebook share

--- src/scripts/app/app.share.js
+++ src/scripts/app/app.share.js
@@ -26,9 +26,9 @@ class mapacheShare {
 
     const socialMedia = {
       facebook: {
-        shareUrl: 'https://www.facebook.com/sharer/sharer.php',
+        shareUrl: 'https://www.facebook.com/sharer.php',
         params: {
-          url: this.mapacheValue('url'),
+          u: this.mapacheValue('url'),
         },
       },
       twitter: {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ Mapache Javascript Functions
 // import external dependencies
 import Prism from 'prismjs';
 import 'prismjs/plugins/autoloader/prism-autoloader';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'sticky-kit/dist/sticky-kit';
 import 'jquery-lazyload';
 
