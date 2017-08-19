f37df8802112e0735db987a50abff970a3d61775
GodoFredo <hell@godofredo.ninja>
2017-08-19 12:26:22 -0500

    add excerpt in article and reorder all

--- src/build/webpack.config.js
+++ src/build/webpack.config.js
@@ -171,10 +171,10 @@ let webpackConfig = {
         eslint: { failOnWarning: false, failOnError: true },
       },
     }),
-    new StyleLintPlugin({
-      failOnError: !config.enabled.watcher,
-      syntax: 'scss',
-    }),
+    // new StyleLintPlugin({
+    //   failOnError: !config.enabled.watcher,
+    //   syntax: 'scss',
+    // }),
   ],
 };
 
