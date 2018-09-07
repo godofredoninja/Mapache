5f1c41c1e97cbdc2110330b839e924ce6716e7c2
GodoFredo <hell@godofredo.ninja>
2018-09-07 11:59:02 -0500

    gallery support :tent

--- src/scripts/routes/post.js
+++ src/scripts/routes/post.js
@@ -47,6 +47,17 @@ export default {
       mapacheInstagram(instagramUserId, instagramToken, instagramUserName); // eslint-disable-line
     }
 
+    // Gallery
+    const images = document.querySelectorAll('.kg-gallery-image img');
+
+    images.forEach(function (image) {
+        const container = image.closest('.kg-gallery-image');
+        const width = image.attributes.width.value;
+        const height = image.attributes.height.value;
+        const ratio = width / height;
+        container.style.flex = ratio + ' 1 0%';
+    })
+
     /* Prism autoloader */
     Prism.highlightAll();
 
