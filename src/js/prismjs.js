a84e479698a9491d7db5c5a01a4ab07e1594638e
GodoFredo <godofredo207@gmail.com>
2019-11-18 15:09:42 -0500

    Updating documentation

--- src/js/prismjs.js
+++
@@ -1,8 +0,0 @@
-/* global sitePrismJscomponents Prism */
-import 'prismjs'
-import 'prismjs/plugins/autoloader/prism-autoloader'
-// import 'prismjs/plugins/line-numbers/prism-line-numbers'
-
-if (typeof sitePrismJscomponents !== 'undefined') {
-  Prism.plugins.autoloader.languages_path = sitePrismJscomponents
-}
